b8af170d40339f4c5f54d613e9a9eb034a3ace2d
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-23 13:36:35 +0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 // Components
+import Nav from './layouts/Nav';
 import Dashboard from './layouts/Dashboard';
+// Semantic-UI
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Nav />
+      <Container className="main">
+        <Dashboard />
+      </Container>
     </div>
   );
 }
