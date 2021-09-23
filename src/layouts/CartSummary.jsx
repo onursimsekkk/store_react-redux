2ba67b4164c026e52f072f83de55b2d4b805181a
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-24 00:06:45 +0300

    Add NavLink for route

--- src/layouts/CartSummary.jsx
+++ src/layouts/CartSummary.jsx
@@ -1,17 +1,20 @@
  import React from 'react'
+ import { NavLink } from 'react-router-dom';
  import { Dropdown } from 'semantic-ui-react';
  
  function CartSummary() {
    return (
-     <div>
-       <Dropdown item text="Language">
+     <>
+       <Dropdown item text="Your Cart">
          <Dropdown.Menu>
            <Dropdown.Item>English</Dropdown.Item>
            <Dropdown.Item>Russian</Dropdown.Item>
            <Dropdown.Item>Spanish</Dropdown.Item>
+           <Dropdown.Divider/>
+           <Dropdown.Item as={NavLink} to="/cart">Go to Cart</Dropdown.Item>
          </Dropdown.Menu>
        </Dropdown>
-     </div>
+     </>
    );
  }
  
