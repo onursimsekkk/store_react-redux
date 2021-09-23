70cfc27ad3a8920b170260ede9f29addf0d82d22
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-24 00:07:13 +0300

    Add components & routes

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Categories from './Categories';
 import ProductList from '../pages/ProductList';
+import ProductDetail from '../pages/ProductDetail';
+import CartDetail from '../pages/CartDetail';
+import { Route } from 'react-router';
 import { Grid } from 'semantic-ui-react';
 
 function Dashboard() {
@@ -12,7 +15,12 @@ function Dashboard() {
             <Categories />
           </Grid.Column>
           <Grid.Column computer={12} mobile={16}>
-            <ProductList />
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/products" component={ProductList} />
+            <Route path="/products/:id" component={ProductDetail} />
+            <Route path="/cart" component={CartDetail} />
+
+
           </Grid.Column>
         </Grid.Row>
       </Grid>
